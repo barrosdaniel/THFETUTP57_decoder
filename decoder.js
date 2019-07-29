a1cb72706604babbba41992725dbb1d64d7b4ed3
Daniel Barros <barros.daniel@outlook.com>
2019-07-29 08:13:55 +0800

    feature: Add decode function

---
+++ decoder.js
@@ -0,0 +1,12 @@
+const key = 'Daniel';
+
+function decode(numbers) {
+  let text = '';
+  for (let i = 0; i < numbers.length; i++) {
+    let letter = key[i].charCodeAt() - 64;
+    text += letter
+  }
+  return text
+}
+
+console.log(decode(key));
